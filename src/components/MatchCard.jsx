d0a8e43fa3dbe39b1cf7fcdc027bcde8772b9b3a
Sosaristic <sundayomena9@gmail.com>
2023-06-26 05:54:37 +0100

    fixed match card undefined

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -1,9 +1,11 @@
 export default function MatchCard({ displayResult, user, setShowUpdateMatchModal, matchesData }) {
+
+  if(matchesData[0] != undefined){
   return (
     <div className="flex flex-col basis-full lg:basis-[45%] items-center shadow-lg font-jost flex-1">
       <p className="bg-dark-green w-full text-center">Round 1</p>
 
-{matchesData[0]?.matches?.map((item, index)=>{
+{  matchesData[0]?.matches?.map((item, index)=>{
   const {home, away} = item
   return  <div key={index} className=" shadow-match-card w-full text-center flex flex-col gap-1 my-2 px-2 py-4 rounded-md">
   {user && (
@@ -25,4 +27,5 @@ export default function MatchCard({ displayResult, user, setShowUpdateMatchModal
 
     </div>
   );
+}
 }
