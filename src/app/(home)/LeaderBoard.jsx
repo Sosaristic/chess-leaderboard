3c93355413369854ab81bf4a5835b5e070a3aded
Sosaristic <sundayomena9@gmail.com>
2023-06-28 10:37:46 +0100

    changed the font size of tbody

--- src/app/(home)/LeaderBoard.jsx
+++ src/app/(home)/LeaderBoard.jsx
@@ -1,49 +1,50 @@
-'use client'
-import useSWR from "swr"
+"use client";
+import useSWR from "swr";
 import { players } from "../../components/data";
 import { getAllPlayers } from "@/services/firestoreFunction";
 export default function LeaderBoard() {
-  const {data, error} = useSWR("teams", getAllPlayers)
+  const { data, error } = useSWR("teams", getAllPlayers);
 
-  if( error){
-    return <div className="text-center mt-4">Error trying to get data</div>
+  if (error) {
+    return <div className="text-center mt-4">Error trying to get data</div>;
   }
-  if(!data){
-    return <div className="text-center mt-4">Fetching data....</div>
+  if (!data) {
+    return <div className="text-center mt-4">Fetching data....</div>;
   }
   return (
     <section className="pb-2">
       <h2 className="text-2xl mt-4 capitalize text-light-green font-[600]">leaderboard 🏆</h2>
-      <table className="w-full border-2 border-dark-green border-spacing-3">
-        <thead className="bg-light-green border">
-          <tr>
-            <th className="p-2">NO</th>
-            <th>Name</th>
-            <th>Games</th>
-<th>W</th>
-<th>D</th>
-<th>L</th>
-            <th>P</th>
-          </tr>
-        </thead>
-        <tbody className="text-center capitalize text-lg ">
-          {data?.map((player, index) => {
-            const { playerName, games, points, w, d, l } = player;
-            return (
-              <tr key={index} className="border border-light-green px-2">
-                <td className="p-2">{index+ 1}</td>
-                <td>{playerName}</td>
-                <td>{games}</td>
-<td>{w}</td>
-<td>{d}</td>
-<td>{l}</td>
-                <td>{points}</td>
-
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="overflow-auto">
+        <table className="w-full border-2 border-dark-green border-spacing-3">
+          <thead className="bg-light-green border">
+            <tr>
+              <th className="p-2">NO</th>
+              <th className="p-2">Name</th>
+              <th className="p-2">Games</th>
+              <th className="p-2">W</th>
+              <th className="p-2">D</th>
+              <th className="p-2">L</th>
+              <th className="p-2">P</th>
+            </tr>
+          </thead>
+          <tbody className="text-center capitalize text-sm ">
+            {data?.map((player, index) => {
+              const { playerName, games, points, w, d, l } = player;
+              return (
+                <tr key={index} className="border border-light-green px-2">
+                  <td className="p-2">{index + 1}</td>
+                  <td className="p-2">{playerName}</td>
+                  <td className="p-2">{games}</td>
+                  <td className="p-2">{w}</td>
+                  <td className="p-2">{d}</td>
+                  <td className="p-2">{l}</td>
+                  <td className="p-2">{points}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
